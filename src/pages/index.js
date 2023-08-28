622030d0e6449adf93fa829067eb49bf84eb5f2f
Raja <s.rajarathinam1999@gmail.com>
2023-08-29 00:08:30 +0530

    project initialisation

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import HomeScreen from "@/components/pages/home/home";
+import CustomContainer from "@/components/ui/custom_container/custom_container";
+
+const Home = () => {
+  return <HomeScreen />;
+};
+
+export default Home;
